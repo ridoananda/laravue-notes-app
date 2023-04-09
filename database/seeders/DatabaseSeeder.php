7b8b25deeea474c37e7a24e30f5a2cdadd1a86a6
Rido Ananda <ridoananda123@gmail.com>
2023-04-09 18:09:57 +0700

    remove zip file and ffix database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+        User::create([
+            'name' => 'Rido Ananda',
+            'email' => 'ridoananda123@gmail.com',
+            'status' => 'Web Developer, Writer',
+            'email_verified_at' => now(),
+            'password' => Hash::make('RidoGans#223303040324'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
